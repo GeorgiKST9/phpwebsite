194fc6e87a88e883a76981d3a3a3ab146419bd1a
GeorgiKST9 <89024335+GeorgiKST9@users.noreply.github.com>
2021-08-16 16:41:48 +0300

    Add files via upload

---
+++ phpweb/about.php
@@ -0,0 +1,27 @@
+<?php
+echo '<!DOCTYPE html>';
+echo '<html lang="en">';
+echo '<head>';
+echo '<title>ABOUT US</title>';
+echo '<link rel="stylesheet" type="text/css" href="css/stylea.css">';
+echo '</head>';
+echo '<body>';
+echo '<header>';
+echo '<div class="main">';
+echo '<ul>';
+echo '<li><a href="index.php">Home</a></li>';
+echo '<li><a href="product.php">Product</a></li>';
+echo '<li><a href="gallery.php">Gallery</a></li>';
+echo '<li><a href="contact.php">Contact</a></li>';
+echo '<li><a href="about.php">About us</a></li>';
+echo '<li><a href="login.php">Login</a></li>';
+echo '</ul>';
+echo '</div>';
+echo '<h1>About us</h1>';
+echo '<div class="container">';
+echo '<p>We are not just smart watch fanatics and techies. We are ordinary people living familiar lives,  merely trying to figure out and find the perfect balance between work, working out, and being social. Just like you, we understand the hardships that come with working a nine-to-five job, juggling busy family schedules and trying to be happy and healthy at the same time. Despite the occasional glass of wine and Netflix binge session, we think we have finally "cracked the code" in how to live our lives with increased ease and maximum efficiency.  Our answer to this -- the bulgarian smart watch!</p>';
+echo '</header>';
+echo '';
+echo '</body>';
+echo '</html>';
+?>
---
+++ phpweb/index.php
@@ -0,0 +1,28 @@
+<?php
+echo '<!DOCTYPE html>';
+echo '<html lang="en">';
+echo '<head>';
+echo '<title>BULGARIAN SMART WATCH</title>';
+echo '<link rel="stylesheet" type="text/css" href="css/style.css">';
+echo '</head>';
+echo '<body>';
+echo '<header>';
+echo '<div class="main">';
+echo '<ul>';
+echo '<li><a href="index.php">Home</a></li>';
+echo '<li><a href="product.php">Product</a></li>';
+echo '<li><a href="gallery.php">Gallery</a></li>';
+echo '<li><a href="contact.php">Contact</a></li>';
+echo '<li><a href="about.php">About us</a></li>';
+echo '<li><a href="login.php">Login</a></li>';
+echo '</ul>';
+echo '</div>';
+echo '<div class="title">';
+echo '<h1>BULGARIAN SMART WATCH</h1>';
+echo '</div>';
+echo '</header>';
+echo '';
+echo '</body>';
+echo '</html>';
+echo '';
+?>
